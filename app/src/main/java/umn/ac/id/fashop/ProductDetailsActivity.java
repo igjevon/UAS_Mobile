99c2c9d1815e98e74e3d8559a11b0285738e272b
jessvaleria <jessvaleria72@gmail.com>
2021-04-28 12:42:38 +0700

    ProductDetails commit

---
+++ app/src/main/java/umn/ac/id/fashop/ProductDetailsActivity.java
@@ -0,0 +1,14 @@
+package umn.ac.id.fashop;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProductDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_details);
+    }
+}
