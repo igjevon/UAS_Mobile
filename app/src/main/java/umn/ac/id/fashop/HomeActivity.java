39957a076c06d0551201528369ae39787cb53f55
jessvaleria <jessvaleria72@gmail.com>
2021-05-02 12:25:52 +0700

    Checkout Commit

--- app/src/main/java/umn/ac/id/fashop/HomeActivity.java
+++ app/src/main/java/umn/ac/id/fashop/HomeActivity.java
@@ -2,13 +2,30 @@ package umn.ac.id.fashop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import umn.ac.id.fashop.ProductDetailsActivity;
+import umn.ac.id.fashop.R;
 
 public class HomeActivity extends AppCompatActivity {
-
+    LinearLayout man_view;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        man_view = findViewById(R.id.man_view);
+        man_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent manIntent = new Intent(HomeActivity.this, ProductDetailsActivity.class);
+                startActivity(manIntent);
+            }
+        });
     }
+
 }
--- app/src/main/java/umn/ac/id/fashop/ProductDetailsActivity.java
+++ app/src/main/java/umn/ac/id/fashop/ProductDetailsActivity.java
@@ -2,13 +2,24 @@ package umn.ac.id.fashop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ProductDetailsActivity extends AppCompatActivity {
-
+    Button addToCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_details);
+        addToCart = findViewById(R.id.addToCart);
+        addToCart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent toCheckout = new Intent(ProductDetailsActivity.this, CheckoutActivity.class);
+                startActivity(toCheckout);
+            }
+        });
     }
 }
