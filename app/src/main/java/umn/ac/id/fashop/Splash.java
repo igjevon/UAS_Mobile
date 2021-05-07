98bf42a63a4398a32edc7af99cb4286c6cfb6f23
Sherina108 <sherina@student.umn.ac.id>
2021-05-07 21:42:47 +0700

    new commit -sher

--- app/src/main/java/umn/ac/id/fashop/Splash.java
+++ app/src/main/java/umn/ac/id/fashop/Splash.java
@@ -3,17 +3,38 @@ package umn.ac.id.fashop;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Splash extends AppCompatActivity {
+    Animation app_splash;
+    ImageView app_logo;
+    TextView app_subtext;
+    View bg1,bg2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        //setting timer untuk 1 detik
+        //load animation
+        app_splash = AnimationUtils.loadAnimation(this, R.anim.app_splash);
+
+        //load element
+        app_logo = findViewById(R.id.app_logo);
+        bg1 = findViewById(R.id.bg1);
+        bg2 = findViewById(R.id.bg2);
+
+        //run animation
+        app_logo.startAnimation(app_splash);
+
+        //setting timer untuk 2 detik
         Handler handler = new Handler();
         handler.postDelayed(new Runnable() {
             @Override
