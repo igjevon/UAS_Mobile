4c8c00f95ebde7a5e4b62f26e7108eade5559860
Sherina108 <sherina@student.umn.ac.id>
2021-05-07 14:12:36 +0700

    new commit -sher

---
+++ app/src/main/java/umn/ac/id/fashop/MyProfileAct.java
@@ -0,0 +1,14 @@
+package umn.ac.id.fashop;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MyProfileAct extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_profile);
+    }
+}
