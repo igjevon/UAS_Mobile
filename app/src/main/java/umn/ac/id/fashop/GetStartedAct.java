3d9126afd676f40292b003482cb8dfbba05cd8a4
Sherina108 <sherina@student.umn.ac.id>
2021-05-07 22:07:12 +0700

    commit animasi getstarted

--- app/src/main/java/umn/ac/id/fashop/GetStartedAct.java
+++ app/src/main/java/umn/ac/id/fashop/GetStartedAct.java
@@ -5,19 +5,36 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class GetStartedAct extends AppCompatActivity {
     Button btn_sign_in;
     Button btn_new_account_create;
+    Animation ttb, btt;
+    ImageView emblem_app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started);
 
+        //load animation
+        ttb = AnimationUtils.loadAnimation(this, R.anim.ttb);
+        btt = AnimationUtils.loadAnimation(this, R.anim.btt);
+
+
+        emblem_app = findViewById(R.id.emblem_app);
         btn_sign_in=findViewById(R.id.btn_sign_in);
         btn_new_account_create=findViewById(R.id.btn_new_account_create);
+
+        //run animation
+        emblem_app.startAnimation(ttb);
+        btn_sign_in.startAnimation(btt);
+        btn_new_account_create.startAnimation(btt);
+
         btn_sign_in.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
