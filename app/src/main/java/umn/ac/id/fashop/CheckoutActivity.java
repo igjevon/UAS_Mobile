8868d484f113b34b506b12b8cbffc6058e6bd01b
jessvaleria <jessvaleria72@gmail.com>
2021-05-09 16:25:22 +0700

    success buy commit

--- app/src/main/java/umn/ac/id/fashop/CheckoutActivity.java
+++ app/src/main/java/umn/ac/id/fashop/CheckoutActivity.java
@@ -2,13 +2,24 @@ package umn.ac.id.fashop;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CheckoutActivity extends AppCompatActivity {
-
+    Button payNow;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+        payNow = findViewById(R.id.payNow);
+        payNow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent successBuyIntent = new Intent(CheckoutActivity.this, SuccessBuyActivity.class);
+                startActivity(successBuyIntent);
+            }
+        });
     }
 }
