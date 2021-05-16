8b92f9f5f9cf578a29684d0fe5b6e888ddcb89c5
Sherina108 <sherina@student.umn.ac.id>
2021-05-16 15:48:11 +0700

    yes commit

--- app/src/main/java/umn/ac/id/fashop/HomeActivity.java
+++ app/src/main/java/umn/ac/id/fashop/HomeActivity.java
@@ -29,7 +29,7 @@ public class HomeActivity extends AppCompatActivity {
     LinearLayout go_to_loungewears;
 
     ImageView pic_photo_home_user_circled;
-    TextView tvNamaPengguna, user_balance;
+    TextView tvNamaPengguna, user_balance, btnMyProfile;
 
     DatabaseReference reference;
 
@@ -47,6 +47,15 @@ public class HomeActivity extends AppCompatActivity {
         pic_photo_home_user_circled = findViewById(R.id.pic_photo_home_user_circled);
         tvNamaPengguna = findViewById(R.id.tvNamaPengguna);
         user_balance = findViewById(R.id.user_balance);
+        btnMyProfile = findViewById(R.id.btnViewProfile);
+
+        btnMyProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent gotoViewProfile = new Intent(HomeActivity.this, MyProfileAct.class);
+                startActivity(gotoViewProfile);
+            }
+        });
 
         reference = FirebaseDatabase.getInstance().getReference()
                 .child("Users").child(username_key_new);
